ee7c0f51486ef157a8675e568eabae8fa1455fc4
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-13 22:31:48 +0530

    Update afk.py

--- EndUserbot/database/afk.py
+++ EndUserbot/database/afk.py
@@ -11,3 +11,14 @@ async def del_afk(self_id: int):
     time = find["time"]
     await afkdb.delete_one({"self_id": self_id, "reason": reason, "time": time})
 
+async def is_afk(self_id: int):
+    find = afkdb.find_one({"self_id": self_id})
+    if find:
+        return True
+    return False
+
+async def get_afk_details(self_id: int):
+    find = afkdb.find_one({"self_id": self_id})
+    reason = find["reason"]
+    time = find["time"]
+    return reason, time
