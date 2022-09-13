6a1307d254b3a62b045fc663aab9ea1618eeaf09
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-13 22:23:59 +0530

    Create afk.py

---
+++ EndUserbot/database/afk.py
@@ -0,0 +1,13 @@
+from ..build_db import db
+
+afkdb = db.afk
+
+async def add_afk(self_id: int, reason: str, time: str):
+    await afkdb.insert_one({"self_id": self_id, "reason": reason, "time": time})
+
+async def del_afk(self_id: int):
+    find = afkdb.find_one({"self_id": self_id})
+    reason = find["reason"]
+    time = find["time"]
+    await afkdb.delete_one({"self_id": self_id, "reason": reason, "time": time})
+
