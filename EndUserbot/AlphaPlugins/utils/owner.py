63c8a19547aaab1c22c2fef8ef92cfc036e7c37a
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-15 22:46:46 +0530

    Update owner.py

--- EndUserbot/AlphaPlugins/utils/owner.py
+++ EndUserbot/AlphaPlugins/utils/owner.py
@@ -1,9 +1,9 @@
-from .details import get_me
+from EndUserbot.database.owner import append_owner
 from pyrogram.types import Message
 from pyrogram import Client
 
-async def owner(_: Client, m: Message):
-    xD = await get_me(_)
-    if m.from_user.id == xD.id:
-        return True
-    return False
+async def load_owner(_: Client):
+    xD = await _.get_me()
+    id = xD.id
+    return append_owner(id)
+    
