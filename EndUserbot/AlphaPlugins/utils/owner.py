c214a843a94c7ab5e3bd2017cc2ac4b69a6661c8
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-15 23:45:34 +0530

    Delete owner.py

--- EndUserbot/AlphaPlugins/utils/owner.py
+++
@@ -1,9 +0,0 @@
-from EndUserbot.database.owner import append_owner
-from pyrogram.types import Message
-from pyrogram import Client
-
-async def load_owner(_: Client):
-    xD = await _.get_me()
-    id = xD.id
-    return await append_owner(id)
-    
