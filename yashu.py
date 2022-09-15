664d2bc9961021c5002210b3d045f9d66d7e3215
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-15 23:46:32 +0530

    Update yashu.py

--- yashu.py
+++ yashu.py
@@ -1,18 +1,10 @@
 from pyrogram import Client, filters, idle
 from config import *
-from EndUserbot.AlphaPlugins.afk import _afk, afk_cwf
-from EndUserbot.AlphaPlugins.utils.owner import load_owner
+
 
 USER = Client(":END-USERBOT:", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
 
-@USER.on_message(filters.command("afk", COMMAND_HANDLER))
-async def afk(_, m):
-    await load_owner(_)
-    await _afk(_, m)
 
-@USER.on_message(group=1)
-async def __afk(_, m):
-    await afk_cwf(_, m)
 
 USER.start()
 get = USER.get_me()
