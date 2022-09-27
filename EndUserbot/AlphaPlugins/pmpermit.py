e667d45e77789669ac1651698a662144959c5dce
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-27 17:02:10 +0530

    Update pmpermit.py

--- EndUserbot/AlphaPlugins/pmpermit.py
+++ EndUserbot/AlphaPlugins/pmpermit.py
@@ -1,2 +1,22 @@
 from . import Client, Message
 from .utils import eor
+from EndUserbot.database import pm_perm, toggle_pm_perm
+from config import COMMAND_HANDLER as hl
+
+async def pm_protect(_, m):
+    if not m.from_user.is_self:
+        return
+    if len(m.command) != 2:
+        return await eor(m, f"<code>{hl}pmpermit ON | OFF</code>")
+    toggler = m.text.split()[1]
+    toggler = toggler.lower()
+    if not toggler in ["on", "off"]:
+        return await eor(m, f"<code>{hl}pmpermit ON | OFF</code>")
+    check = await pm_perm()
+    if check:
+        if toggler == "on":
+            return await eor(m, f"<i>pm protection is already enabled..!</i>")
+        await toggle_pm_perm()
+        return await eor(m, f"<i>pm protection enabled..!</i>")
+    else:
+        if toggler == "off":
