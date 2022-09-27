bd31ba081741798a544f45ae1222b0d1c5f08585
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-27 17:49:45 +0530

    Update pm.py

--- EndUserbot/database/pm.py
+++ EndUserbot/database/pm.py
@@ -15,3 +15,16 @@ async def toggle_pm_perm():
     if is_on:
         return await pmdb.delete_one({"pm_perm": 1})
     
+async def get_pm_warns(user_id: int):
+    hehe = await pmdb.find_one({"user_id": user_id})
+    if not hehe:
+        return 0
+    else:
+        warns = hehe["warns"]
+        return int(warns)
+
+async def warn_user(user_id: int):
+    lmao = await get_pm_warns(user_id)
+    await pmdb.delete_one({"user_id"})
+    lmao = lmao + 1
+    return await pmdb.insert_one({"user_id": user_id}, {"$set": {"warns": lmao}})
