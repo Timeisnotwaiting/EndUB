b53a5bb1c5c77b1d8603b10b5c64522e8ad9bc10
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-27 16:46:58 +0530

    Update Pm.py

--- EndUserbot/database/Pm.py
+++ EndUserbot/database/Pm.py
@@ -3,4 +3,15 @@ from ..build_db import db
 pmdb = db.pm
 
 async def pm_perm():
+    on = pmdb.find_one({"pm_perm": 1})
+    if not on:
+        return False
+    return True
+
+async def toggle_pm():
+    is_on = await pm_perm()
+    if not is_on:
+        return await pmdb.insert_one({"pm_perm": 1})
+    if is_on:
+        return await pmdb.delete_one({"pm_perm": 1})
     
