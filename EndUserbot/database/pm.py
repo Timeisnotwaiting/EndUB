0240ab916b3477644453c7ee5b4d817b29dfd3b1
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-28 12:20:50 +0530

    Update pm.py

--- EndUserbot/database/pm.py
+++ EndUserbot/database/pm.py
@@ -2,6 +2,8 @@ from ..build_db import db
 
 pmdb = db.pm
 
+pma = db.pm_approve
+
 async def pm_perm():
     on = pmdb.find_one({"pm_perm": 1})
     if not on:
@@ -28,3 +30,16 @@ async def warn_user(user_id: int):
     await pmdb.delete_one({"user_id"})
     lmao = lmao + 1
     return await pmdb.insert_one({"user_id": user_id}, {"$set": {"warns": lmao}})
+
+async def is_approved(user_id: int):
+    app = pma.find_one({"user_id": user_id})
+    if app:
+        return True
+    return False
+
+async def toggle_approve(user_id: int):
+    is_app = await is_approved(user_id)
+    if is_app:
+        return await pma.delete_one({"user_id": user_id})
+    else:
+        return await pma.insert_one({"user_id": user_id})
