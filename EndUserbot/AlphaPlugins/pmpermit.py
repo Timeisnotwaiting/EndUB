0cf0199fa23bcf03d22f267dd99afa695012c9e2
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-28 12:44:10 +0530

    Update pmpermit.py

--- EndUserbot/AlphaPlugins/pmpermit.py
+++ EndUserbot/AlphaPlugins/pmpermit.py
@@ -1,6 +1,6 @@
 from . import Client, Message
-from .utils import eor
-from EndUserbot.database import pm_perm, toggle_pm_perm
+from .utils import eor, get_id
+from EndUserbot.database import pm_perm, toggle_pm_perm, is_approved, toggle_approve
 from config import COMMAND_HANDLER as hl
 
 async def pm_protect(_, m):
@@ -30,7 +30,30 @@ async def pm_cwf(_, m):
         return
     if m.chat.type != "private":
         return 
+    check_user = await is_approved(m.chat.id)
+    if check_user:
+        return
     await warn_user(m.chat.id)
     w = await get_pm_warns(m.chat.id)
     if w == 3:
     
+
+async def approve_disapprove(_, m):
+    if not m.from_user.is_self:
+        return
+    try:
+        id = await get_id(m)
+    except:
+        return await eor(m, f"<i>{hl}a | {hl}da [ID | USERNAME | REPLY]</i>")
+    approved = await is_approved(id)
+    if m.text.split()[0][1].lower() == "d":
+        if not approved:
+            return await eor(m, f"<i>user wasn't approved..!</i>")
+        await toggle_approve(id)
+        return await eor(m, f"<i>user disapproved..!</i>")
+    else:
+        if approved:
+            return await eor(m, f"<i>user is already approved...!</i>")
+        await toggle_approve(id)
+        return await eor(m, f"<i>user approved..!</i>")
+        
