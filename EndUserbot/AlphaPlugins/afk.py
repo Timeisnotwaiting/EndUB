e7f0b0dd4a2a03c35a72d9b075c075911514babe
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-10-01 13:46:33 +0530

    Update afk.py

--- EndUserbot/AlphaPlugins/afk.py
+++ EndUserbot/AlphaPlugins/afk.py
@@ -4,6 +4,29 @@ from ..database import add_afk, remove_afk, is_afk, get_afk_details
 import time 
 from .utils import eor
 
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for i in range(len(time_list)):
+        time_list[i] = str(time_list[i]) + time_suffix_list[i]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
+
 
 async def set_afk(_, m):
     if not m.from_user.is_self:
