15022e9f970b8a9212d8b17be22cec4c999cdc42
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-10-04 01:00:56 +0530

    Update type.py

--- EndUserbot/AlphaPlugins/type.py
+++ EndUserbot/AlphaPlugins/type.py
@@ -2,8 +2,14 @@ from . import Client, Message
 import time
 from .utils import eor
 from config import COMMAND_HANDLER as hl
+from ..database import is_sudo
 
 async def type(_, m):
+    check = await is_sudo(m.from_user.id)
+    l = await _.get_me()
+    id = l.id
+    if not m.from_user.id == id and not check:
+        return
     if len(m.command) < 2:
         return await eor(m, f"{hl}TYPE TEXT") 
     txt = m.text.split(None, 1)[1]
