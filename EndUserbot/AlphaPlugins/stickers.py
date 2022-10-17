46e01ed41cae7522fe101112be2f9f8f8f3b1332
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-10-17 13:43:46 +0530

    Update stickers.py

--- EndUserbot/AlphaPlugins/stickers.py
+++ EndUserbot/AlphaPlugins/stickers.py
@@ -1,7 +1,16 @@
-from .utils import eor, get_reply_and_message
+from .utils import eor, get_reply_and_message, verify
 from . import Client, Message
 import asyncio
 from ..database import is_sudo
+from pathlib import Path
+
+def conv(s):
+    des = s.with_suffix(".webp")
+    
 
 async def kang(_, m):
-    id = m.from_user.id
+    v = await verify(_, m, m.from_user.id)
+    if not v:
+        return
+    reply, message = await get_reply_and_message(m)
+    
