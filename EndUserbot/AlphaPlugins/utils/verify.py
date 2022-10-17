47611d824f73f8388c05298220f792b054d6a30f
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-10-17 21:42:50 +0530

    Update verify.py

--- EndUserbot/AlphaPlugins/utils/verify.py
+++ EndUserbot/AlphaPlugins/utils/verify.py
@@ -1,11 +1,14 @@
 from ...database import is_sudo
 from ..import Client, Message
 
-async def verify(_: Client, m: Message, id: None):
-    id = m.from_user.id
-    sudo = await is_sudo(m.from_user.id)
-    myself = await _.get_me().id
-    self_ = True if myself == id else False
-    if not self_ and not sudo:
-        return False
-    return True
+async def verify(id):
+    global id
+    async def verify_user(_: Client, m: Message):
+        sudo = await is_sudo(m.from_user.id)
+        global id
+        myself = await _.get_me().id
+        self_ = True if myself == id else False
+        if not self_ and not sudo:
+            return False
+        return True
+    return await verify_user(_, m)
