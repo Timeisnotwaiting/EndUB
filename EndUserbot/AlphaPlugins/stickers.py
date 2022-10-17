0c66fc0dca0d6bef389ddb2ff918b319125a53d1
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-10-17 21:32:03 +0530

    Update stickers.py

--- EndUserbot/AlphaPlugins/stickers.py
+++ EndUserbot/AlphaPlugins/stickers.py
@@ -94,7 +94,7 @@ async def kang(_, m):
                 await _.send_message("stickers", "/skip")
                 await asyncio.sleep(0.2)
                 await _.send_message("stickers", un)
-                return await eor(m, "STICKER KANGED [PACK](f"https://t.me/addstickers/{pn}")
+                return await eor(m, f"STICKER KANGED [PACK](https://t.me/addstickers/{pn})")
             except:
                 return await eor(m, "ERROR OCCURED")
         else:
@@ -108,6 +108,6 @@ async def kang(_, m):
                 await _.send_message("stickers", emoji)
                 await asyncio.sleep(0.2)
                 await _.send_message("stickers", "/done")
-                return await eor(m, "STICKER KANGED [PACK](f"https://t.me/addstickers/{pn}")
+                return await eor(m, f"STICKER KANGED [PACK](https://t.me/addstickers/{pn})")
             except:
                 return await eor(m, "ERROR OCCURED")
