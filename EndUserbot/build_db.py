546b20fcd4fa29009d41cf37a9f4dde50343914d
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-10-17 20:38:57 +0530

    Update build_db.py

--- EndUserbot/build_db.py
+++ EndUserbot/build_db.py
@@ -1,6 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-import config 
+from config import MONGO_DB_URL
 
-mongo = MongoClient(config.MONGO_DB_URL)
+if not MONGO_DB_URL:
+    from yashu import MONGO_DB_URL 
+
+mongo = MongoClient(MONGO_DB_URL)
 
 db = mongo.END
