c919ad5f304c1f9939207e82f0000f53f6df4923
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-10-23 10:18:45 +0530

    Update memify.py

--- EndUserbot/AlphaPlugins/memify.py
+++ EndUserbot/AlphaPlugins/memify.py
@@ -4,128 +4,20 @@ from ..database import is_sudo
 from PIL import Image, ImageFont, ImageDraw
 import os
 
-async def drawText(image_path, text):
-    img = Image.open(image_path)
-    os.remove(image_path)
-    shadowcolor = "black"
-    i_width, i_height = img.size
-    fnt = "ariel.ttf"
-    m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
-    if ";" in text:
-        upper_text, lower_text = text.split(";")
-    else:
-        upper_text = text
-        lower_text = ""
-    draw = ImageDraw.Draw(img)
-    current_h, pad = 10, 5
-    if upper_text:
-        for u_text in textwrap.wrap(upper_text, width=15):
-            u_width, u_height = draw.textsize(u_text, font=m_font)
-            draw.text(
-                xy=(((i_width - u_width) / 2) - 2, int((current_h / 640) * i_width)),
-                text=u_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(((i_width - u_width) / 2) + 2, int((current_h / 640) * i_width)),
-                text=u_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=((i_width - u_width) / 2, int(((current_h / 640) * i_width)) - 2),
-                text=u_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(((i_width - u_width) / 2), int(((current_h / 640) * i_width)) + 2),
-                text=u_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
+def DrawSize(file_name):
+    x = Image.open(file_name)
+    size = x.size
+    return size
 
-            draw.text(
-                xy=((i_width - u_width) / 2, int((current_h / 640) * i_width)),
-                text=u_text,
-                font=m_font,
-                fill=(255, 255, 255),
-            )
-            current_h += u_height + pad
-    if lower_text:
-        for l_text in textwrap.wrap(lower_text, width=15):
-            u_width, u_height = draw.textsize(l_text, font=m_font)
-            draw.text(
-                xy=(
-                    ((i_width - u_width) / 2) - 2,
-                    i_height - u_height - int((20 / 640) * i_width),
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(
-                    ((i_width - u_width) / 2) + 2,
-                    i_height - u_height - int((20 / 640) * i_width),
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(
-                    (i_width - u_width) / 2,
-                    (i_height - u_height - int((20 / 640) * i_width)) - 2,
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-            draw.text(
-                xy=(
-                    (i_width - u_width) / 2,
-                    (i_height - u_height - int((20 / 640) * i_width)) + 2,
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(0, 0, 0),
-            )
-
-            draw.text(
-                xy=(
-                    (i_width - u_width) / 2,
-                    i_height - u_height - int((20 / 640) * i_width),
-                ),
-                text=l_text,
-                font=m_font,
-                fill=(255, 255, 255),
-            )
-            current_h += u_height + pad
-    image_name = "memify.webp"
-    webp_file = os.path.join(image_name)
-    img.save(webp_file, "webp")
-    return webp_file
-
-async def memify_event(_, m):
-    xD = m.from_user.is_self
-    check = await is_sudo(m.from_user.id)
-    if not xD and not check:
-        return 
-    if not m.reply_to_message:
-        return await eor(m, f"reply to a sticker..!")
-    if not m.reply_to_message.sticker:
-        return await eor(m, f"reply to a sticker..!")
-    if len(m.command) == 1:
-        return await eor(m, f"give some text to memify..!")
-    await eor(m, f"memefying..!")
-    text = str(m.text.split(None, 1)[1])
-    file = await _.download_media(m.reply_to_message, file_name=f"{m.from_user.id}.webp")
-    file_path = f"downloads/{m.from_user.id}.webp"
-    final = await drawText(file_path, text)
-    await m.delete()
-    await _.send_sticker(m.chat.id, final)
-    os.remove(final)
-    return 
-    
+async def mmf(_, m):
+    id = m.from_user.id
+    sd = await is_sudo(id)
+    x = await _.get_me()
+    i = x.id
+    if not sd and id != i:
+        return
+    reply, message = await get_reply_and_message(_, m)
+    if not reply in ["photo", "sticker"]:
+        return await eor(m, "REPLY TO A STICKER OR AN IMAGE !!")
+    if not message:
+        return await eor(m, "GIVE SOME TEXT TO DO SO !!")
