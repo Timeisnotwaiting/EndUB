e6c371f257c109226454c8919c5ed3c97e87e45e
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-12-05 19:40:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import getenv as _
 
-API_ID = int(_("API_ID", None))
-API_HASH = _("API_HASH", None)
+API_ID = int(_("API_ID", 10763476))
+API_HASH = _("API_HASH", "e7d6d5493a896264a09d04fda7a30f9d")
 STRING_SESSION = _("STRING_SESSION", None)
-MONGO_DB_URL = _("MONGO_DB_URL", None)
+MONGO_DB_URL = _("MONGO_DB_URL", "mongodb+srv://musicbot:<password>@cluster0.61lydz4.mongodb.net/?retryWrites=true&w=majority")
 COMMAND_HANDLER = _("COMMAND_HANDLER", None)
-ALIVE_PIC = _("ALIVE_PIC", None)
+ALIVE_PIC = _("ALIVE_PIC", "https://te.legra.ph/file/be4de09294b646d9479ee.jpg")
 
 if not COMMAND_HANDLER:
     COMMAND_HANDLER = "!"
